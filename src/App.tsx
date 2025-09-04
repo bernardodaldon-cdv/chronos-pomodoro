0f6f7097479832c37802688122d8e50fedb6d7f5
Bernardo Daldon <bernardo.daldon@casadosventos.com.br>
2025-09-04 15:09:16 -0300

    clean original app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/theme.css'
+import './styles/global.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App() {
+    console.log('Oi');
+  
+    return (
+        <>
+            <h1>Hello World! (do APP)</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel pariatur hic sed nesciunt, numquam a iure quas ratione eius est fuga, ex exercitationem officia necessitatibus aliquid harum omnis, qui sint.
+            </p>
+        </>
+    );
+    //Sempre precisa de div contornando tudo, só manda um pai, pode ser só
+    //um <> e </> (fragment)
 }
 
-export default App
+//export default App;  
+//export { App };
